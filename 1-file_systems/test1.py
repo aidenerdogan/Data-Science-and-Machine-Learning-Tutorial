679aca85cc098e1f04911cb3255c7d37715ba895
ahmterdgn903 <erdoganahmt.1903@gmail.com>
2020-01-29 13:02:03 +0530

    29.01.2020

--- 1-file_systems/test1.py
+++ 1-file_systems/test1.py
@@ -1,28 +1,60 @@
 import csv
+# importing required modules 
+from zipfile import ZipFile as zf
 
+# specifying the zip file name 
+file_path = 'YouTube-Spam-Collection-v1.zip'
+
+# read and  extract file from zip file exercise
+# try:
+# 	with zf(file_path,'r') as zip_file:
+# 		# printing all the contents of the zip file 
+# 		zip_file.printdir()
+# 		print(type(zip_file))
+# 		# for i in zip_file.extractall():
+# 		# 	l1.append(i)
+# 	# 	for i in list(zip_file):
+# 	# 		l1.append(i)
+# 	# print(l1)
+# except FileExistsError  as e:
+# 	print(e)
+
+def get_file():
+	l1 = []
+	count = 0
+	# opening the zip file in READ mode 
+	with zf(file_path,'r') as zip_file:
+		for i in zip_file.namelist():
+			if count>0:
+				l1.append(i)
+			count +=1
+	return l1
+for i in get_file():
+	print(i)
 def get_data(file):
 	l1 = []
-	with open(file,'r',newline='') as cvs_file:
-		csv_reader = csv.reader(cvs_file,delimiter=',')
+	# read data from csv file
+	with open(file,'r') as csv_file:
+		csv_reader = csv.reader(csv_file)
 		for i in csv_reader:
 			l1.append(i)
-			# print(i)
-	return l1
-def insert_data(file,mode,data):
-    with open(file,mode,newline='') as f:
-        # creating a csv dict writer object
-        writer = csv.writer(f,delimiter=',')
-        writer.writerows(data)
+	return l1[:2]
 
-reading_f1 = 'SalesJan2009.csv'
-reading_f2= 'TechCrunchcontinentalUSA.csv'
-writing_f1 = 'test4.csv'
-writing_f2 = 'test5.csv'
+def insert_data(file_name):
+	# insert data to csv file
+	with open(file_name,'w') as csv_file:
+		csv_writer = csv.writer(csv_file)
+		# insert data to csv file from csv files in zip file
+		for i in get_file():
+			csv_writer.writerows(get_data(i))
 
-# get_data('TechCrunchcontinentalUSA.csv')
-insert_data(writing_f1,'w',get_data(reading_f1))
-insert_data(writing_f2,'w',get_data(reading_f2))
+# insert_data('test5.csv')
+for i in get_file():
+	print(get_data(i))
 
+# https://www.google.com/search?q=extract+zip+file+python&oq=extract+xip+file+pythn&aqs=chrome.1.69i57j0l7.9183j0j7&sourceid=chrome&ie=UTF-8
+
+# https://www.pythonforthelab.com/blog/storing-data-with-sqlite/
 # print(get_data('test1.csv'))
 # Task:
 #      create multiple csv files with some data and compress it in zip format
