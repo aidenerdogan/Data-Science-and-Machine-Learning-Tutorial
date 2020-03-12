bf977df5bc22e359e5d717a8374084d0b4c80f8f
ahmterdgn903 <erdoganahmt.1903@gmail.com>
2020-03-12 19:09:26 +0530

    12.03.20

--- prject/model4_logic.py
+++ prject/model4_logic.py
@@ -1,123 +1,153 @@
-import csv,re, random,math
+import csv,re,random
+# stopwords list
 stopwords = ["wouldn't", 'own', 'ours', 'under', "who's", 'also', "here's", 'could', 'have', 'but', 'com', 'ourselves', "where's", 'at', "they'd", 'hence', 'doing', "you're", "how's", 'r', 'yours', 'against', 'same', "i'd", 'which', 'more', 'very', 'whom', 'ought', 'http', 'for', 'was', 'most', 'of', 'his', 'are', 'so', 'did', 'am', "she'll", 'been', 'up', 'does', 'after', 'few', 'until', "they're", 'it', 'both', 'their', 'the', "there's", "can't", 'into', "he's", 'they', 'through', "why's", 'get', 'all', 'them', 'herself', "we'll", 'if', 'can', 'out', "i'm", 'and', 'because', "it's", "they've", "isn't", 'were', 'some', 'than', "wasn't", 'too', "that's", "they'll", 'that', "haven't", 'such', "i've", 'while', 'why', 'like', 'should', 'to', "you'll", 'hers', 'you', 'then', 'however', 'down', 'before', "she's", 'any', "what's", "he'd", 'here', 'www', "didn't", "you've", 'cannot', 'above', 'her', 'from', "shan't", "you'd", 'yourselves', 'an', "aren't", 'only', 'theirs', "we'd", 'on', 'having', 'each', 'else', 'who', 'or', "i'll", 'had', 'as', "mustn't", "couldn't", "weren't", 'me', 'further', 'over', 'where', 'when', 'no', 'being', "she'd", 'about', 'those', 'in', 'she', "when's", 'has', 'its', 'how', "don't", "hadn't", 'just', "we're", 'him', 'himself', 'once', 'he', 'since', 'again', 'my', 'this', "we've", 'off', 'would', 'not', 'these', 'your', 'therefore', 'themselves', 'our', 'is', 'between', 'ever', "won't", "he'll", 'i', 'do', "let's", 'with', "hasn't", 'k', 'otherwise', "doesn't", 'we', 'by', 'itself', 'shall', 'be', "shouldn't", 'below', 'during', 'myself', 'nor', 'what', 'other', 'there', 'yourself', 'a', 'mightn', 'mustn', 't', "mightn't", 'haven', 'don', 'isn', 's', 'wouldn', 'will', 'needn', 'hadn', 'll', 'couldn', 'aren', 'wasn', 'ain', 'm', 'didn', 'won', "that'll", "should've", "needn't", 're', 'o', 'doesn', 've', 'y', 'ma', 'now', 'd', 'shan', 'shouldn', 'hasn', 'weren']
 # GET TRAIN DATA
 # data info
 # We are using labelled word for train model
 # this data contains 160154 negative,90710 positive words
 def get_train_data():
+	positive_words,negative_words = [],[]
 	# first 35 lines for data info
 	for word in open('positive_words.txt','r',encoding='ISO-8859-1').readlines()[35:]:
 		word = word.replace('\n', '').lower()
-		# if word in positive_words_counts:
-		# 	positive_words_counts[word] += 1
-		# else:
-		# 	positive_words_counts[word] = 1
 		positive_words.append([word.rstrip(),1.0])
 	for word in open('negative_words.txt', 'r', encoding = "ISO-8859-1").readlines()[35:]:
 		word = word.replace('\n', '').lower()
-		# if word in negative_words_counts:
-		# 	negative_words_counts[word] += 1
-		# else:
-		# 	negative_words_counts[word] = 1
 		negative_words.append([word.rstrip(),0.0])
-	# data = positive_words + negative_words
+	or_data = positive_words + negative_words
+	data = random.sample(or_data, len(or_data))
+	return data
 
-		# [print(row) for row in csv_reader]
-#Veri setini train,test ayrımı
-def split_data(data):
-    trainSize = int(len(data) * 0.7)
+# TRAIN MODEL
+# split data to training and test data via split_ratio
+def split_data_set(data_set, split_ratio):
+    trainSize = int(len(data_set) * split_ratio)
     trainSet = []
-    copy = list(data)
+    copy = list(data_set)
     while len(trainSet) < trainSize:
         index = random.randrange(len(copy))
         trainSet.append(copy.pop(index))
-    # print(trainSet)
-    return [trainSet, copy]
-def seperateByClass(data):
-    seperated = {}
-    for i in range(len(data)):
-        vector = data[i]
-        # print('vector[-1]',vector[-1])
-        if (vector[-1] not in seperated):
-            seperated[vector[-1]] = []
-        seperated[vector[-1]].append(vector)
-    # print(seperated)
-    return seperated
+    return [trainSet, copy]	
 
-# def get():
-# 	for word in trainingSet:
-# 		if word[1] == 1.0:
-# 			if word[0] in positive_words_counts:
-# 				positive_words_counts[word[0]] += 1
-# 			else:
-# 				positive_words_counts[word[0]] = 1
-# 		else:
-# 			if word[0] in negative_words_counts:
-# 				negative_words_counts[word[0]] += 1
-# 			else:
-# 				negative_words_counts[word[0]] = 1
-
-# TRAIN MODEL
-# we are using BernoulliNB of Naive Bayes Classification algorithm for train model
-def get_result():
-	test_data = testSet
-	# print(test_data)
-	True_Pos = 0
-	True_Neg = 0
-	results = []
-	# print(test_data)
-	for word in test_data:
-		# print(word[0])
+# get all polarities from data
+def get_probability(data_test):
+	# step 1 get P_class
+	for word in training_set:
 		if word[1] == 1.0:
-			# print(word[0])
-			if word[0] in positive_words:
-				if word[0] in negative_words:
-					# print(word[0])
-					result = ((positive_words_counts[word[0]]/len(positive_words))*(len(positive_words)/(len(positive_words)+len(negative_words))))/((positive_words_counts[word[0]]+negative_words_counts[word[0]])/(len(positive_words)+len(negative_words)))
-					# print(result)
-				else:
-					result = ((positive_words_counts[word[0]]/len(positive_words))*(len(positive_words)/(len(positive_words)+len(negative_words))))/((positive_words_counts[word[0]]+0)/(len(positive_words)+len(negative_words)))
-				# print(result)
-				if float(result)>0.7:
-					# print(result)
-					True_Pos += 1
-					# print(True_Pos)
-				else:
-					True_Neg += 1
-		elif word[1] == 1.0:
+			if word[0] in positive_words_counts:
+					positive_words_counts[word[0]] += 1
+			else:
+					positive_words_counts[word[0]] = 1
+		else:
 			if word[0] in negative_words_counts:
-				result = None
-				if word[0] in positive_words:
-					# print(word[0])
-					result = ((negative_words_counts[word[0]]/negative_words)*(len(negative_words)/(len(positive_words)+len(negative_words))))/((positive_words_counts[word[0]]+negtative_words_counts[word[0]])/(len(positive_words)+len(negative_words)))
-				else:
-					result = ((negative_words_counts[word[0]]/negative_words)*(len(negative_words)/(len(positive_words)+len(negative_words))))/((0+negtative_words_counts[word[0]])/(len(positive_words)+len(negative_words)))
+				negative_words_counts[word[0]] += 1
+			else:
+				negative_words_counts[word[0]] = 1
+	# laplace_smooting(data_test)
+	P_positive = sum(positive_words_counts.values())/len(training_set)
+	P_negative = sum(negative_words_counts.values())/len(training_set)
+	# step 2 get P_words
+	P_positive_words = dict()
+	P_negative_words = dict()
+	for word in positive_words_counts.keys():
+		P_positive_words[word] = positive_words_counts[word]/len(training_set)
+	for word in negative_words_counts.keys():
+		P_negative_words[word] = negative_words_counts[word]/len(training_set)
+	return P_positive,P_negative,P_positive_words,P_negative_words
+def laplace_smooting(test_set):
+	for word in test_set:
+		if word[0] not in positive_words_counts:
+			for word2 in positive_words_counts:
+				positive_words_counts[word2] += 1
+			positive_words_counts[word[0]] = 1
+		if word[0] not in negative_words_counts:
+			for word2 in negative_words_counts:
+				negative_words_counts[word2] += 1
+			negative_words_counts[word[0]] = 1
 
-				# print(result)
-				if float(result)>0.7:
-					# print(result)
-					True_Neg += 1
-				else:
-					True_Pos += 1
-					# print(True_Neg)
-	print(True_Pos, True_Neg)
-	accuracy = (True_Pos+True_Neg)/(len(positive_words)+len(negative_words))
-	print(accuracy)
+def get_predict(test_set):
+	predict = []
+	for word in test_set:
+		p_word_positive,p_word_negative = 0.0,0.0
+		if word[0] in P_positive_words:
+			p_word_positive = (P_positive_words[word[0]]*P_positive)/(P_positive_words[word[0]]+P_negative_words[word[0]])
+		else:
+			pass
+		if word[0] in P_negative_words:
+			p_word_negative = (P_negative_words[word[0]]*P_negative)/(P_positive_words[word[0]]+P_negative_words[word[0]])
+		else:
+			pass
+		if p_word_positive > p_word_negative:
+			predict.append([word[0],1.0])
+		else:
+			predict.append([word[0],0.0])
+	return predict
+
+def get_accuracy(predict):
+	count = 0
+	TN,TP,FN,FP = 0,0,0,0
+	for i in range(len(test_set)):
+		if test_set[i][1] == predict[i][1]:
+			if predict[i][1] == 1.0:
+				TP += 1
+			else:
+				TN += 1
+			count += 1
+		else:
+			if predict[i][1] == 0.0:
+				FN += 1
+			else:
+				FP += 1 
+	accuracy1 = (TN+TP)/(TN+TP+FN+FP)
+	accuracy = count/len(test_set)
+	preccision = TP/(FP+TP)
+	specificity = TN/(TN+FP)
+	sensivity = TP/(TP+FN)
+	print('accuracy1',accuracy1)
+	print('accuracy',accuracy)
+	print('preccision',preccision)
+	print('sepcificion',specificity)
+	print('sensivity',sensivity)
 	# return accuracy
+def sentiment_text(text):
+	words =[word.lower() for word in (re.findall(r"[\w']+|[.,!?;]", text.rstrip())) if len(word) >= 3 and word not in stopwords]
+	print(words)
+	return words
+def get_sentiment(words):
+	result = None
+	p_word_positive,p_word_negative = 0.0,0.0
+	# get_probability(words)
+	for word in words:
+		if word in P_positive_words:
+			p_word_positive = (P_positive_words[word]*P_positive)/(P_positive_words[word]+P_negative_words[word])
+			# p_word_positive = p_word_positive*P_positive_words[word]
+		else:
+			p_word_positive = p_word_positive*1
+		if word in P_negative_words:
+			p_word_negative = (P_negative_words[word]*P_negative)/(P_positive_words[word]+P_negative_words[word])
+		else:
+			p_word_negative = p_word_negative*1
+	p_word_positive = p_word_positive*P_positive
+	p_word_negative = p_word_negative*P_negative
+	if p_word_positive > p_word_negative:
+		result = 'positive'
+	else:
+		result = 'negative'
+	print(result)
+	return result
 
-
-# def get_features():
-# 	P_positive = len(positive_words)/(len(positive_words)+len(negative_words))
-# 	P_negative = len(negative_words)/(len(positive_words)+len(negative_words))
-# 	print(P_positive, P_negative)
-
-# get_test_data('Sentiment.csv')
 if __name__ == '__main__':
-	positive_words = []
-	negative_words = []
+	data_set = get_train_data()
 	positive_words_counts = dict()
 	negative_words_counts = dict()
-	get_train_data()
-	data = positive_words + negative_words
-	trainingSet, testSet = split_data(data)
-	print('Split {0} row into train = {1} and test {2} rows'.format(len(data),len(trainingSet),len(testSet)))
+	training_set, test_set = split_data_set(data_set,0.67)
+	print('Split {0} row into train = {1} and test {2} rows'.format(len(data_set),len(training_set),len(test_set)))
+	P_positive, P_negative,P_positive_words,P_negative_words = get_probability(test_set)
+	# print(P_positive_words)
+	# print(P_negative_words)
+	text = input('pls input a text :')
+	get_predict(test_set)
+	get_accuracy(get_predict(test_set))
+	# sentiment_text(text)
+	# laplace_smooting(sentiment_text(text))
+	get_sentiment(sentiment_text(text))
--- prject/test.py
+++ prject/test.py
@@ -1,125 +1,146 @@
-import csv
-import math
-import random
+import csv,re,random
+# stopwords list
+stopwords = ["wouldn't", 'own', 'ours', 'under', "who's", 'also', "here's", 'could', 'have', 'but', 'com', 'ourselves', "where's", 'at', "they'd", 'hence', 'doing', "you're", "how's", 'r', 'yours', 'against', 'same', "i'd", 'which', 'more', 'very', 'whom', 'ought', 'http', 'for', 'was', 'most', 'of', 'his', 'are', 'so', 'did', 'am', "she'll", 'been', 'up', 'does', 'after', 'few', 'until', "they're", 'it', 'both', 'their', 'the', "there's", "can't", 'into', "he's", 'they', 'through', "why's", 'get', 'all', 'them', 'herself', "we'll", 'if', 'can', 'out', "i'm", 'and', 'because', "it's", "they've", "isn't", 'were', 'some', 'than', "wasn't", 'too', "that's", "they'll", 'that', "haven't", 'such', "i've", 'while', 'why', 'like', 'should', 'to', "you'll", 'hers', 'you', 'then', 'however', 'down', 'before', "she's", 'any', "what's", "he'd", 'here', 'www', "didn't", "you've", 'cannot', 'above', 'her', 'from', "shan't", "you'd", 'yourselves', 'an', "aren't", 'only', 'theirs', "we'd", 'on', 'having', 'each', 'else', 'who', 'or', "i'll", 'had', 'as', "mustn't", "couldn't", "weren't", 'me', 'further', 'over', 'where', 'when', 'no', 'being', "she'd", 'about', 'those', 'in', 'she', "when's", 'has', 'its', 'how', "don't", "hadn't", 'just', "we're", 'him', 'himself', 'once', 'he', 'since', 'again', 'my', 'this', "we've", 'off', 'would', 'not', 'these', 'your', 'therefore', 'themselves', 'our', 'is', 'between', 'ever', "won't", "he'll", 'i', 'do', "let's", 'with', "hasn't", 'k', 'otherwise', "doesn't", 'we', 'by', 'itself', 'shall', 'be', "shouldn't", 'below', 'during', 'myself', 'nor', 'what', 'other', 'there', 'yourself', 'a', 'mightn', 'mustn', 't', "mightn't", 'haven', 'don', 'isn', 's', 'wouldn', 'will', 'needn', 'hadn', 'll', 'couldn', 'aren', 'wasn', 'ain', 'm', 'didn', 'won', "that'll", "should've", "needn't", 're', 'o', 'doesn', 've', 'y', 'ma', 'now', 'd', 'shan', 'shouldn', 'hasn', 'weren']
+# GET TRAIN DATA
+# data info
+# We are using labelled word for train model
+# this data contains 160154 negative,90710 positive words
+def get_train_data():
+    positive_words,negative_words = [],[]
+    # first 35 lines for data info
+    for word in open('positive_words.txt','r',encoding='ISO-8859-1').readlines()[35:]:
+        word = word.replace('\n', '').lower()
+        positive_words.append([word.rstrip(),1.0])
+    for word in open('negative_words.txt', 'r', encoding = "ISO-8859-1").readlines()[35:]:
+        word = word.replace('\n', '').lower()
+        negative_words.append([word.rstrip(),0.0])
+    or_data = positive_words + negative_words
+    data = random.sample(or_data, len(or_data))
+    return data
 
-# Veri setinin yüklenmesi
-def loadCsv(filename):
-    lines= csv.reader(open('pimaindiansdiabetescsv/pima-indians-diabetes.csv'))
-    dataset = list(lines)
-    for i in range(len(dataset)):
-        dataset[i] = [float(x) for x in dataset[i]]
-    # print(dataset[1])
-    return dataset
-
-#Veri setini train,test ayrımı
-def splitDataset(dataset, splitRatio):
-    trainSize = int(len(dataset) * splitRatio)
+# TRAIN MODEL
+# split data to training and test data via split_ratio
+def split_data_set(data_set, split_ratio):
+    trainSize = int(len(data_set) * split_ratio)
     trainSet = []
-    copy = list(dataset)
+    copy = list(data_set)
     while len(trainSet) < trainSize:
         index = random.randrange(len(copy))
         trainSet.append(copy.pop(index))
-    # print(trainSet)
-    return [trainSet, copy]
+    return [trainSet, copy] 
 
-# Her sınıf kendi içerisinde değerlendirilir. Sınıfları birbirinden ayırdık.
-def seperateByClass(dataset):
-    seperated = {}
-    for i in range(len(dataset)):
-        vector = dataset[i]
-        # print('vector[-1]',vector[-1])
-        if (vector[-1] not in seperated):
-            seperated[vector[-1]] = []
-        seperated[vector[-1]].append(vector)
-    # print(seperated)
-    return seperated
+# get all polarities fro data
+def get_probablity(data_test):
+    # step 1 get P_class
+    for word in training_set:
+        if word[1] == 1.0:
+            if word[0] in positive_words_counts:
+                    positive_words_counts[word[0]] += 1
+            else:
+                    positive_words_counts[word[0]] = 1
+        else:
+            if word[0] in negative_words_counts:
+                negative_words_counts[word[0]] += 1
+            else:
+                negative_words_counts[word[0]] = 1
+    laplace_smooting(data_test)
+    P_positive = sum(positive_words_counts.values())/len(training_set)
+    P_negative = sum(negative_words_counts.values())/len(training_set)
+    # step 2 get P_words
+    P_positive_words = dict()
+    P_negative_words = dict()
+    for word in positive_words_counts.keys():
+        P_positive_words[word] = positive_words_counts[word]/len(training_set)
+    for word in negative_words_counts.keys():
+        P_negative_words[word] = negative_words_counts[word]/len(training_set)
+    return P_positive,P_negative,P_positive_words,P_negative_words
+def laplace_smooting(test_set):
+    for word in test_set:
+        if word[0] not in positive_words_counts:
+            for word2 in positive_words_counts:
+                positive_words_counts[word2] += 1
+            positive_words_counts[word[0]] = 1
+        if word[0] not in negative_words_counts:
+            for word2 in negative_words_counts:
+                negative_words_counts[word2] += 1
+            negative_words_counts[word[0]] = 1
 
-#Ortalama
-def mean(numbers):
-    # print(numbers)
-    return sum(numbers)/float(len(numbers))
+def get_predict(test_set):
+    predict = []
+    for word in test_set:
+        p_word_positive,p_word_negative = 0.0,0.0
+        p_word_positive = P_positive_words[word[0]]*P_positive
+        p_word_negative = P_negative_words[word[0]]*P_negative
+        if p_word_positive > p_word_negative:
+            predict.append([word[0],1.0])
+        else:
+            predict.append([word[0],0.0])
+    return predict
 
-# Standart Sapma
-def stdev(numbers):
-    avg = mean(numbers)
-    variance = sum([pow(x-avg,2) for x in numbers]) / float(len(numbers) -1)
-    return math.sqrt(variance)
+def get_accuracy(predict):
+    count = 0
+    TN,TP,FN,FP = 0,0,0,0
+    for i in range(len(test_set)):
+        if test_set[i][1] == predict[i][1]:
+            if predict[i][1] == 1.0:
+                TP += 1
+            else:
+                TN += 1
+            count += 1
+        else:
+            if predict[i][1] == 0.0:
+                FN += 1
+            else:
+                FP += 1 
+    accuracy1 = (TN+TP)/(TN+TP+FN+FP)
+    accuracy = count/len(test_set)
+    preccision = TP/(FP+TP)
+    specificity = TN/(TN+FP)
+    sensivity = TP/(TP+FN)
+    print('accuracy1',accuracy1)
+    print('accuracy',accuracy)
+    print('preccision',preccision)
+    print('sepcificion',specificity)
+    print('sensivity',sensivity)
+    # return accuracy
+def sentiment_text(text):
+    words =[word.lower() for word in (re.findall(r"[\w']+|[.,!?;]", text.rstrip())) if len(word) >= 3 and word not in stopwords]
+    print(words)
+    return words
+def get_sentiment(words):
+    result = None
+    p_word_positive,p_word_negative = 0.0,0.0
+    # get_probablity(words)
+    for word in words:
+        if word in P_positive_words:
+            p_word_positive = p_word_positive*P_positive_words[word]
+        else:
+            p_word_positive = p_word_positive*1
+        if word in P_negative_words:
+            p_word_negative = p_word_negative*P_negative_words[word]
+        else:
+            p_word_negative = p_word_negative*1
+    p_word_positive = p_word_positive*P_positive
+    p_word_negative = p_word_negative*P_negative
+    if p_word_positive > p_word_negative:
+        result = 'positive'
+    else:
+        result = 'negative'
+    print(result)
+    return result
 
-# Tüm featurelar için Ortalama ve Standart sapma 
-def summarize(dataset):
-    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-    # print([att for att in zip(*dataset)])
-    del summaries[-1]
-    return summaries
-
-#Sınıflarda aynı işlemlerin yapılması
-def summerizeByClass(dataset):
-    seperated = seperateByClass(dataset)
-    summaries = {}
-    for classValue,instances in seperated.items():
-        summaries[classValue]  = summarize(instances)
-    return summaries
-
-# Normal Dağılımı hesaplama
-def calculateProbability(x,mean,stdev):
-    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-    return (1/ (math.sqrt(2*math.pi)*stdev))*exponent
-
-#  Sınıf Değeri için olasılıklar
-def calculateClassProbabilities(summaries, inputVector):
-    probabilities = {}
-    # print(summaries)
-    for classValue,classSummaries in summaries.items():
-        # print(classValue, classSummaries)
-        probabilities[classValue]  = 1
-        for i in range(len(classSummaries)):
-            mean,stdev = classSummaries[i]
-            x = inputVector[i]
-            probabilities[classValue] *= calculateProbability(x, mean, stdev)
-        # print(probabilities)
-        return probabilities
-
-# Tahminler
-def predict(summaries, inputVector):
-    probabilities = calculateClassProbabilities(summaries,inputVector)
-    # print(probabilities)
-    bestLabel, bestProb = None, -1
-    for classValue, probability in probabilities.items():
-        if bestLabel is None or probability > bestProb:
-            bestProb = property
-            bestLabel = classValue
-    # print(bestLabel, bestProb)
-    return bestLabel
-
-# Test verisinde tahminlerin yapılması
-def getPredictions(summaries, testSize):
-    predictions  = []
-    for i in range(len(testSize)):
-        result = predict(summaries, testSize[i])
-        predictions.append(result)
-        # print(result)
-    return predictions
-
-# Başarı testi
-def getAccuracy(testSet,predictions):
-    correct = 0
-    for x in range(len(testSet)):
-        if testSet[x][-1] == predictions[x]:
-            correct +=1
-    return (correct/float(len(testSet)))*100.0
-
-def main():
-    filename = 'pima-indians-diabetes.csv'
-    splitRatio = 0.67
-    dataset = loadCsv(filename)
-    #print(dataset)
-    trainingSet, testSet = splitDataset(dataset, splitRatio)
-    print('Split {0} row into train = {1} and test {2} rows'.format(len(dataset),len(trainingSet),len(testSet)))
-    #prepare model
-    summaries = summerizeByClass(trainingSet)
-    #test model
-    predictions = getPredictions(summaries,testSet)
-    accuracy = getAccuracy(testSet , predictions)
-    print('Accuracy %{0}'.format(accuracy))
-    
-main()
+if __name__ == '__main__':
+    data_set = get_train_data()
+    positive_words_counts = dict()
+    negative_words_counts = dict()
+    training_set, test_set = split_data_set(data_set,0.67)
+    print('Split {0} row into train = {1} and test {2} rows'.format(len(data_set),len(training_set),len(test_set)))
+    P_positive, P_negative,P_positive_words,P_negative_words = get_probablity(test_set)
+    # print(P_positive_words)
+    # print(P_negative_words)
+    text = input('pls input a text :')
+    get_predict(test_set)
+    get_accuracy(get_predict(test_set))
+    # sentiment_text(text)
+    # laplace_smooting(sentiment_text(text))
+    get_sentiment(sentiment_text(text))
